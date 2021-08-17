318b98236b4b101ec3a1b401a8ea11e31fb4e0d8
walrusfromspace <walrusfromspace@tutanota.com>
2021-08-17 13:20:41 +0000

    Added comments

--- rps.c
+++ rps.c
@@ -6,29 +6,28 @@
 enum hand{ROCK=0, PAPER, SCISSORS};
 
 int main() {
-	int pHand = -1;
-	int cHand;
-	int inpt;
-	srand((unsigned long)time(NULL));
-	while(pHand == -1){
+	int pHand = -1; // Player's hand
+	int cHand;	// The computer's hand
+	int inpt;	// input
+	srand((unsigned long)time(NULL)); // Let's seed rand() with current unix time in seconds
+	while(pHand == -1){ // Loop until we receive valid input
 		printf("Please choose one of the following: 1) Rock, 2) Paper or 3) Scissors\n:");
-		inpt = (int)getc(stdin) - 49;
-		if(inpt < 0 || inpt > 2)
+		inpt = (int)getc(stdin) - 49; // Char is converted into a integer and has the value of '1' removed
+		if(inpt < 0 || inpt > 2) // If we did not receive valid input inform the player
 			printf("Unknown input.\n");
 		else
-			pHand = inpt;
+			pHand = inpt; // If we received valid input change the players hand to input
 	}
-	cHand = rand() % 3;
-	if(pHand > cHand || (pHand == ROCK && cHand == SCISSORS)){
+	cHand = rand() % 3; // Get a random number in the range of 0..2
+	if(pHand > cHand || (pHand == ROCK && cHand == SCISSORS)){ // 0 beats 2 and 2 beats 1 and 1 beats 0
 		printf("You win!\n");
 		return 0;
-	} else if (pHand == cHand) {
+	} else if (pHand == cHand) { // The result is a tie if both hands are equal
 		printf("Tie!\n");
 		return 0;
-	} else {
+	} else { // In the case the result is not a tie the player loses
 		printf("You lose!\n");
 		return 0;
 	}
-	return 1;
+	return 1; // If we did not fullfill any of the conditions something went wrong
 }
-
