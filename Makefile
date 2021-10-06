9ba8d6df711c9830d0168c10321cfaf31db81be5
walrusfromspace <walrusfromspace@tutanota.com>
2021-10-06 10:22:41 +0000

    Small fixes

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CFLAGS=-std=c99 -Wall -Werror -Wextra -Wpedantic -O2
 LDFLAGS=
 
 compile:
-	${CC} ${CFLAGS} -o rps rpsc.c ${LDFLAGS}
+	${CC} ${CFLAGS} -o rps rps.c ${LDFLAGS}
 
 clean:
 	rm rps
--- rps.c
+++ rps.c
@@ -11,7 +11,7 @@ int main() {
 	int inpt;	// input
 	srand((unsigned long)time(NULL)); // Let's seed rand() with current unix time in seconds
 	while(pHand == -1){ // Loop until we receive valid input
-		printf("Please choose one of the following: 1) Rock, 2) Paper or 3) Scissors\n:");
+		printf("Please choose one of the following: 1) Rock, 2) Paper or 3) Scissors\n");
 		inpt = (int)getc(stdin) - 49; // Char is converted into a integer and has the value of '1' removed
 		if(inpt < 0 || inpt > 2) // If we did not receive valid input inform the player
 			printf("Unknown input.\n");
@@ -19,6 +19,12 @@ int main() {
 			pHand = inpt; // If we received valid input change the players hand to input
 	}
 	cHand = rand() % 3; // Get a random number in the range of 0..2
+	if(cHand == ROCK)
+		printf("The enemy chose ROCK!\n");
+	else if(cHand == SCISSORS)
+		printf("The enemy chose SCISSORS!\n");
+	else
+		printf("The enemy chose PAPER!\n");
 	if(pHand > cHand || (pHand == ROCK && cHand == SCISSORS)){ // 0 beats 2 and 2 beats 1 and 1 beats 0
 		printf("You win!\n");
 		return 0;
